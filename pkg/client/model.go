b2116a928dae77cf85e36df5d4a057e2dec443f4
mchavez <miguel_chavez_m@hotmail.com>
2024-07-08 17:56:24 -0600

    Updating data model

--- pkg/client/model.go
+++ pkg/client/model.go
@@ -1,83 +1,83 @@
 package client
 
 type NodesAPIData struct {
-	Class    string     `json:"_class"`
-	Computer []Computer `json:"computer"`
+	Class    string     `json:"_class,omitempty"`
+	Computer []Computer `json:"computer,omitempty"`
 }
 
 type Computer struct {
-	Class               string           `json:"_class"`
-	AssignedLabels      []AssignedLabels `json:"assignedLabels"`
-	Description         string           `json:"description"`
-	DisplayName         string           `json:"displayName"`
-	Idle                bool             `json:"idle"`
-	ManualLaunchAllowed bool             `json:"manualLaunchAllowed"`
+	Class               string           `json:"_class,omitempty"`
+	AssignedLabels      []AssignedLabels `json:"assignedLabels,omitempty"`
+	Description         string           `json:"description,omitempty"`
+	DisplayName         string           `json:"displayName,omitempty"`
+	Idle                bool             `json:"idle,omitempty"`
+	ManualLaunchAllowed bool             `json:"manualLaunchAllowed,omitempty"`
 }
 
 type AssignedLabels struct {
-	Name string `json:"name"`
+	Name string `json:"name,omitempty"`
 }
 
 type JobsAPIData struct {
-	Class string `json:"_class"`
-	Jobs  []Job  `json:"jobs"`
+	Class string `json:"_class,omitempty"`
+	Jobs  []Job  `json:"jobs,omitempty"`
 }
 
 type Job struct {
-	Class     string `json:"_class"`
-	Name      string `json:"name"`
-	URL       string `json:"url"`
-	Buildable bool   `json:"buildable"`
-	Color     string `json:"color"`
+	Class     string `json:"_class,omitempty"`
+	Name      string `json:"name,omitempty"`
+	URL       string `json:"url,omitempty"`
+	Buildable bool   `json:"buildable,omitempty"`
+	Color     string `json:"color,omitempty"`
 }
 
 type ViewsAPIData struct {
-	Class string `json:"_class"`
-	Views []View `json:"views"`
+	Class string `json:"_class,omitempty"`
+	Views []View `json:"views,omitempty"`
 }
 
 type View struct {
-	Class string `json:"_class"`
-	Name  string `json:"name"`
-	URL   string `json:"url"`
+	Class string `json:"_class,omitempty"`
+	Name  string `json:"name,omitempty"`
+	URL   string `json:"url,omitempty"`
 }
 
 type UsersAPIData struct {
-	Class string  `json:"_class"`
-	Users []Users `json:"users"`
+	Class string  `json:"_class,omitempty"`
+	Users []Users `json:"users,omitempty"`
 }
 
 type Users struct {
-	LastChange interface{} `json:"lastChange"`
-	Project    interface{} `json:"project"`
-	User       User        `json:"user"`
+	LastChange interface{} `json:"lastChange,omitempty"`
+	Project    interface{} `json:"project,omitempty"`
+	User       User        `json:"user,omitempty"`
 }
 
 type User struct {
-	AbsoluteURL string      `json:"absoluteUrl"`
-	Description interface{} `json:"description"`
-	FullName    string      `json:"fullName"`
-	ID          string      `json:"id"`
+	AbsoluteURL string      `json:"absoluteUrl,omitempty"`
+	Description interface{} `json:"description,omitempty"`
+	FullName    string      `json:"fullName,omitempty"`
+	ID          string      `json:"id,omitempty"`
 }
 
 type RolesAPIData struct {
-	RoleName   string `json:"RoleName"`
-	RoleDetail []Role `json:"roles"`
+	RoleName   string `json:"RoleName,omitempty"`
+	RoleDetail []Role `json:"roles,omitempty"`
 }
 
 type Role struct {
-	Sid  string `json:"sid"`
-	Type string `json:"type"`
+	Sid  string `json:"sid,omitempty"`
+	Type string `json:"type,omitempty"`
 }
 
 type Group struct {
-	AbsoluteURL string      `json:"absoluteUrl"`
-	Description interface{} `json:"description"`
-	FullName    string      `json:"fullName"`
-	ID          string      `json:"id"`
+	AbsoluteURL string      `json:"absoluteUrl,omitempty"`
+	Description interface{} `json:"description,omitempty"`
+	FullName    string      `json:"fullName,omitempty"`
+	ID          string      `json:"id,omitempty"`
 }
 
 type GroupsAPIData struct {
-	Class  string  `json:"_class"`
-	Groups []Group `json:"groups"`
+	Class  string  `json:"_class,omitempty"`
+	Groups []Group `json:"groups,omitempty"`
 }
