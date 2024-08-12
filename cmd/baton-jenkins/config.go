838129af7dcf8c964d31f12a98e72de6177e86a4
Jorge Javier Araya Navarro <jorge@esavara.cr>
2024-08-12 15:55:48 -0600

    Migrate configuration

--- cmd/baton-jenkins/config.go
+++ cmd/baton-jenkins/config.go
@@ -1,40 +1,20 @@
 package main
 
 import (
-	"context"
-	"fmt"
-
-	"github.com/conductorone/baton-sdk/pkg/cli"
+	"github.com/conductorone/baton-sdk/pkg/field"
 )
 
-// config defines the external configuration required for the connector to run.
-type config struct {
-	cli.BaseConfig  `mapstructure:",squash"` // Puts the base config options in the same place as the connector options
-	JenkinsUsername string                   `mapstructure:"jenkins-username" description:"Username of administrator used to connect to the Jenkins API."`
-	JenkinsPassword string                   `mapstructure:"jenkins-password" description:"Application password used to connect to the Jenkins API."`
-	JenkinstBaseUrl string                   `mapstructure:"jenkins-baseurl" description:"Jenkins. example http://localhost:8080." defaultValue:"http://localhost:8080"`
-	JenkinsToken    string                   `mapstructure:"jenkins-token" description:"HTTP access tokens in Jenkins"`
+var (
+	username = field.StringField("username", field.WithDescription("Username of administrator used to connect to the Jenkins API"))
+	password = field.StringField("password", field.WithDescription("Application password used to connect to the Jenkins API"))
+	baseUrl  = field.StringField("base-url", field.WithDescription("Jenkins"), field.WithDefaultValue("http://localhost:8080"), field.WithRequired(true))
+	token    = field.StringField("token", field.WithDescription("HTTP access tokens in Jenkins"))
+)
+
+var relationships = []field.SchemaFieldRelationship{
+	field.FieldsMutuallyExclusive(token, username),
+	field.FieldsMutuallyExclusive(token, password),
+	field.FieldsRequiredTogether(username, password),
 }
 
-// validateConfig is run after the configuration is loaded, and should return an error if it isn't valid.
-func validateConfig(ctx context.Context, cfg *config) error {
-	if cfg.JenkinstBaseUrl == "" {
-		return fmt.Errorf("jenkins-baseurl must be provided")
-	}
-
-	if cfg.JenkinsUsername == "" {
-		return fmt.Errorf("jenkins-username must be provided")
-	}
-
-	if cfg.JenkinsToken == "" {
-		if cfg.JenkinsUsername == "" || cfg.JenkinsPassword == "" {
-			return fmt.Errorf("either bitbucketdc-token or (bitbucketdc-username/bitbucketdc-password) must be provided")
-		}
-	}
-
-	if cfg.JenkinsToken != "" && cfg.JenkinsUsername != "" && cfg.JenkinsPassword != "" {
-		return fmt.Errorf("bitbucketdc-token, and (bitbucketdc-username/bitbucketdc-password) cannot be provided simultaneously")
-	}
-
-	return nil
-}
+var configuration = field.NewConfiguration([]field.SchemaField{username, password, baseUrl, token}, relationships...)
