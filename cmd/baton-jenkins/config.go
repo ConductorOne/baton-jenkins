e5c89ad332dc12497b4cf5e91afc190fb6282435
Jorge Javier Araya Navarro <jorge@esavara.cr>
2024-08-13 15:55:20 -0600

    Make `username` required

--- cmd/baton-jenkins/config.go
+++ cmd/baton-jenkins/config.go
@@ -5,7 +5,7 @@ import (
 )
 
 var (
-	username = field.StringField("username", field.WithDescription("Username of administrator used to connect to the Jenkins API"))
+	username = field.StringField("username", field.WithDescription("Username of administrator used to connect to the Jenkins API"), field.WithRequired(true))
 	password = field.StringField("password", field.WithDescription("Application password used to connect to the Jenkins API"))
 	baseUrl  = field.StringField("base-url", field.WithDescription("Jenkins"), field.WithDefaultValue("http://localhost:8080"), field.WithRequired(true))
 	token    = field.StringField("token", field.WithDescription("HTTP access tokens in Jenkins"))
